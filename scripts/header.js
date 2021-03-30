c85ae8c46cc4ea9588a37a1b3849ced135c7c645
Rahul Rana <rahulrana585@gmail.com>
2021-03-30 13:31:35 +0530

    .

--- scripts/header.js
+++ scripts/header.js
@@ -5,12 +5,14 @@ function openSignIn() {
 function openSignUp() {
     document.getElementById('signUpModal').style.display='block';
 }
+  
 
-function onCloseModalClicked() {
+function forSignUpButton(){
+    document.getElementById('signUpModal').style.display='block';
+}
+
+function forCloseModalClicked() {
     document.getElementById('signInModal').style.display='none';
     document.getElementById('signUpModal').style.display='none';
 }
 
-function openSignUpButton(){
-    document.getElementById('signUpModal').style.display='block';
-}
